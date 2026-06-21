26b57c4e6a5d2e287092f72397f3f87b27e0d434
Pedro H. Sauné <pedrohsaune@gmail.com>
2026-06-21 18:59:32 -0300

    Separate public exports by submodule

--- src/pyramid/__init__.py
+++ src/pyramid/__init__.py
@@ -1,51 +1,21 @@
 # Auto generated 'src/pyramid/__init__.py':
 
-from .asset import AssetManager
-from .asset import BoundCache
-from .audio import AudioManager
-from .audio import Track
-from .camera import Camera2D
-from .compile import CythonCompiler
-from .grid import GridManager
-from .save import JSONStore
-from .scene import Scene
-from .scene import SceneHaltSignal
-from .scene import SceneManager
-from .ui import ImageScaleMode
-from .ui import PanelOrientation
-from .ui import UIAlignmentH
-from .ui import UIAlignmentV
-from .ui import UIButton
-from .ui import UIElement
-from .ui import UIImage
-from .ui import UIPanel
-from .ui import UISize
-from .ui import UIStyle
-from .ui import UITextBox
-from .ui import UITextElement
+from . import asset
+from . import audio
+from . import camera
+from . import compile
+from . import grid
+from . import save
+from . import scene
+from . import ui
 
 __all__ = [
-    "AssetManager",
-    "AudioManager",
-    "BoundCache",
-    "Camera2D",
-    "CythonCompiler",
-    "GridManager",
-    "ImageScaleMode",
-    "JSONStore",
-    "PanelOrientation",
-    "Scene",
-    "SceneHaltSignal",
-    "SceneManager",
-    "Track",
-    "UIAlignmentH",
-    "UIAlignmentV",
-    "UIButton",
-    "UIElement",
-    "UIImage",
-    "UIPanel",
-    "UISize",
-    "UIStyle",
-    "UITextBox",
-    "UITextElement",
+    "asset",
+    "audio",
+    "camera",
+    "compile",
+    "grid",
+    "save",
+    "scene",
+    "ui",
 ]
