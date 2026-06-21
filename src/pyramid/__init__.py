5fa72921baebb877b6628994c710d9190c84a560
Pedro H. Sauné <pedrohsaune@gmail.com>
2026-06-21 17:37:49 -0300

    Fix packaging

---
+++ src/pyramid/__init__.py
@@ -0,0 +1,51 @@
+# Auto generated 'src/pyramid/__init__.py':
+
+from src.pyramid.asset import AssetManager
+from src.pyramid.asset import BoundCache
+from src.pyramid.audio import AudioManager
+from src.pyramid.audio import Track
+from src.pyramid.camera import Camera2D
+from src.pyramid.compile import CythonCompiler
+from src.pyramid.grid import GridManager
+from src.pyramid.save import JSONStore
+from src.pyramid.scene import Scene
+from src.pyramid.scene import SceneHaltSignal
+from src.pyramid.scene import SceneManager
+from src.pyramid.ui import ImageScaleMode
+from src.pyramid.ui import PanelOrientation
+from src.pyramid.ui import UIAlignmentH
+from src.pyramid.ui import UIAlignmentV
+from src.pyramid.ui import UIButton
+from src.pyramid.ui import UIElement
+from src.pyramid.ui import UIImage
+from src.pyramid.ui import UIPanel
+from src.pyramid.ui import UISize
+from src.pyramid.ui import UIStyle
+from src.pyramid.ui import UITextBox
+from src.pyramid.ui import UITextElement
+
+__all__ = [
+    "AssetManager",
+    "AudioManager",
+    "BoundCache",
+    "Camera2D",
+    "CythonCompiler",
+    "GridManager",
+    "ImageScaleMode",
+    "JSONStore",
+    "PanelOrientation",
+    "Scene",
+    "SceneHaltSignal",
+    "SceneManager",
+    "Track",
+    "UIAlignmentH",
+    "UIAlignmentV",
+    "UIButton",
+    "UIElement",
+    "UIImage",
+    "UIPanel",
+    "UISize",
+    "UIStyle",
+    "UITextBox",
+    "UITextElement",
+]
